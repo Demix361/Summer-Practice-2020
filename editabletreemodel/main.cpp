64fc1c0c8d2a1c59c798857259c2f76a2ad571ec
Demix361 <ti-aminov@yandex.ru>
2020-07-22 13:23:28 +0300

    delete done

---
+++ editabletreemodel/main.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QtSql>
+#include <QDebug>
+
+
+int main(int argc, char *argv[])
+{
+    Q_INIT_RESOURCE(editabletreemodel);
+
+    QSqlDatabase sdb = QSqlDatabase::addDatabase("QSQLITE");
+    sdb.setDatabaseName("testdb.db");
+    if (!sdb.open()) {
+        qDebug() << sdb.lastError().text();
+    }
+
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+    return app.exec();
+}
